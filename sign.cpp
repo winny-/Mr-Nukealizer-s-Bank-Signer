e4e76b8b9d3c0f5449e07cbf2b836d183201f0ce
Winston Weinert <winston@ml1.net>
2014-02-02 08:01:01 -0600

    Oops. Fixed the reason why it does not appear to work.

--- sign.cpp
+++ sign.cpp
@@ -9,12 +9,13 @@ int main(int argc, char *argv[])
 	std::ifstream ifs(argv[1]);
 	char oldSignature[100] = "";
 	char newSignature[100] = "";
+	char name[100] = "";
 	std::string content( (std::istreambuf_iterator<char>(ifs) ),
 		(std::istreambuf_iterator<char>()    ) );
 
 	GetCurrentSig(content.c_str(), oldSignature, sizeof(oldSignature));
-	// GetNewSig(const char* authorAccountNumber, const char* userAccountNumber, const char* bankName, const char* bankData, char* sigBuffer, int destinationSize)
-	GetNewSig(argv[2], argv[3], argv[1], content.c_str(), newSignature, sizeof(newSignature));
+	//        author   user     name     data             buf           bufsize
+	GetNewSig(argv[2], argv[3], argv[4], content.c_str(), newSignature, sizeof(newSignature));
 
 	std::cout << "Old signature: " << oldSignature << std::endl;
 	std::cout << "New signature: " << newSignature << std::endl;
